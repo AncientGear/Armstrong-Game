a1435ac6969a06055c5ac10c8c1d023652c568db
SaulTzakumOchoa <cpp.saul@gmail.com>
2020-02-03 15:23:39 -0600

    My first game in  Global Game Jam

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+
+    float currentTime;
+    public float startTime;
+    private bool paused = false;
+
+    public Text countdownText;
+     public Text wintext;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(paused == false){
+            currentTime -= 1 * Time.deltaTime;
+            countdownText.text = currentTime.ToString("0");
+            if (currentTime <= 0)
+            {
+            currentTime = 0;
+             wintext.text = "GameOver";
+            }
+
+        }
+        else{
+            currentTime = currentTime;
+        }
+
+    }
+    public void AddBonusTime(float bonusTime)
+    {
+        currentTime += bonusTime;
+    }
+
+    public void PauseToggle()
+    {
+        if(paused== true){ 
+            paused = false; 
+        }
+        else
+        {
+            paused = true;
+        }
+    }
+}
+
---
+++ Assets/Scripts/verifyWin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class verifyWin : MonoBehaviour, IInteractable
+{
+    private GameObject InventorySlots;
+    public GameObject uiObject;
+    public int counter;
+    public Text wintext;
+
+    
+    public void Interact(DisplayImage currentDisplay)
+    {
+        verify();
+   }
+
+    public void verify(){
+        counter=0;
+        InventorySlots = GameObject.Find("Slots");
+
+        foreach(Transform slot in InventorySlots.transform){
+            if(slot.transform.GetChild(0).GetComponent<Image>().sprite.name != "empty_item"){
+                counter++;
+            }
+        }
+
+        if(counter == 6){
+            Debug.Log("Has ganado! ;)");
+            wintext.text = "You Win";
+           // GameObjectVariable.GetComponent<CountdownTimer>().PauseToggle();
+        }
+    }
+}
